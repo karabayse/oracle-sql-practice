a11a971ff25819ade00f08cfcf5f6d7d3aec9f25
Kara Bayse <karabayse@gmail.com>
2019-03-18 16:51:45 -0500

    create table

--- oracle.sql
+++ oracle.sql
@@ -1,3 +1,17 @@
-SELECT * FROM temporary_table_a
-
-INSERT row_a, row_b
+CREATE TABLE dummy_table
+(
+  Parent_Key  Number(22) NOT NULL
+  Surrogate_Key  Number(22) NOT NULL
+  dummy_column_1  VARCHAR2(255)
+)
+tablepsace  DATA3
+pctfree  10
+initrans  2
+maxtrans  255
+storage (
+  initial  64K
+  next  1M
+  minextents  1
+  maxextents  unlimited
+)
+compress;
