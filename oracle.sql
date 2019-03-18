712018cf2e157388536adfd55452e4dc9a888ffc
Kara Bayse <karabayse@gmail.com>
2019-03-18 16:54:49 -0500

    alter table

--- oracle.sql
+++ oracle.sql
@@ -15,3 +15,18 @@ storage (
   maxextents  unlimited
 )
 compress;
+-- create or recreate primary, unique, and foreign key constraints
+ALTER TABLE dummy_table
+  add constraint y_y_y
+    PRIMARY KEY(Parent_Key, Surrogate_Key)
+  using  INDEX
+  tablespace  INDEX 3
+  pctfree  10
+  initrans  2
+  maxtrans  255
+  storage (
+    initial  64K
+    next  1M
+    minextents  1
+    maxextents  unlimited
+  );
